60a1e3105429520d01c7bd7c64cf8b89528eee83
Virgili Daniel <virgilidaniel@gmail.com>
2024-10-07 14:48:06 +1000

    Session Changes

--- sessionCheck.php
+++ sessionCheck.php
@@ -10,9 +10,9 @@ if (isset($_SESSION['user_id'])) {
     $response['status'] = 'logged_in';
     $response['user_id'] = $_SESSION['user_id'];
     $response['user_type'] = $_SESSION['user_type']; // Assuming user_type is saved in the session
-    $response['user_email'] = $_SESSION['email']; // Add more session details if needed
-    $response['user_firstname'] = $_SESSION['firstName'];
-    $response['user_lastname'] = $_SESSION['lastName'];
+    $response['user_email'] = $_SESSION['user_email']; // Add more session details if needed
+    $response['user_firstname'] = $_SESSION['user_firstname'];
+    $response['user_lastname'] = $_SESSION['user_lastname'];
 } else {
     // If not logged in
     $response['status'] = 'not_logged_in';
