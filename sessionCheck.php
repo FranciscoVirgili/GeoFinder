f4f3cca9cff680e27147ec9822b1378865bf6f26
Virgili Daniel <virgilidaniel@gmail.com>
2024-10-07 14:06:22 +1000

    Session Changes

--- sessionCheck.php
+++ sessionCheck.php
@@ -1,5 +1,5 @@
 <?php
-//session_start();
+session_start();
 
 // Check if the user is logged in
 if (isset($_SESSION['user_id'])) {
@@ -9,4 +9,23 @@ if (isset($_SESSION['user_id'])) {
     // Return a JSON response indicating the user is not logged in
     echo json_encode(['status' => 'not_logged_in']);
 }
+
+// Sample session check logic
+$response = [];
+
+    if (isset($_SESSION['user_id'])) {
+        $response['user_id'] = $_SESSION['user_id'];
+        $response['status'] = 'logged_in';
+        $response['user_type'] = ['user_type']; // Assuming user_type is saved in the session
+        $response['email'] = $_SESSION['email']; // You can add more session details here
+        $response['first_name'] = $_SESSION['firstName'];
+        $response['last_name'] = $_SESSION['lastName'];
+        
+    } else {
+        $response['status'] = 'not_logged_in';
+    }
+
+    // Return session details as JSON response
+    header('Content-Type: application/json');
+    echo json_encode($response);
 ?>
