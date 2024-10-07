eae17c2ed96911af80a83dc2b89ed8ab88ba38d3
Virgili Daniel <virgilidaniel@gmail.com>
2024-10-07 14:54:09 +1000

    Session Changes

--- sessionCheck.js
+++ sessionCheck.js
@@ -1,15 +1,15 @@
 // sessionCheck.js
 
 // Check user session via AJAX
-function checkUserSession(userTypeRequired, redirectIfNotMatch = 'signin.html') {
+function checkUserSession(userTypeRequired, redirectIfNotMatch = 'unauthorized.html') {
     fetch('sessionCheck.php')
         .then(response => response.json())
         .then(data => {
-            
+
             console.log(data);  // <-- This will log the entire JSON response to the console
             if (data.status === 'not_logged_in') {
                 // Redirect to the sign-in page if not logged in
-                window.location.href = 'signin.html';
+                window.location.href = redirectIfNotMatch;
             } else if (data.user_type !== userTypeRequired) {
                 // If logged in but not the required user type, redirect to a different page
                 window.location.href = redirectIfNotMatch;
